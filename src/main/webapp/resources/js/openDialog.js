411f4df83fb3a174e2e27bf0c942188c808a4758
lars <lars.andren@keypasco.com>
2015-09-16 09:16:53 +0200

    Added text capture.

--- src/main/webapp/resources/js/openDialog.js
+++ src/main/webapp/resources/js/openDialog.js
@@ -1,40 +1,65 @@
-//Initialize dialog
-$("#dialog").dialog({
-  autoOpen: false,
-  show: {
-    effect: "blind",
-    duration: 1000
-  },
-  hide: {
-    effect: "explode",
-    duration: 1000
+
+$(function() {
+
+  // Allows user to click Enter key in text field and it will submit the dialog
+  $('#reportDialog').keypress(function(e) {
+    if (e.keyCode == $.ui.keyCode.ENTER) {
+      getResponse();
+    }
+  });
+
+  var cancel = function() {
+    $("#reportDialog").dialog("close");
   }
+
+  var getResponse = function() {
+
+    var answer;
+    answer = $("#correction").val();
+
+    $("#result").text(answer);
+    $("#reportDialog").dialog("close");
+  }
+
+  var dialogOpts = {
+    dialogClass : 'no-close',
+    height : 300,
+    width : 300,
+    scrollable : true,
+    modal : true,
+    closeOnEscape : true,
+    buttons : {
+      "Done" : getResponse,
+      "Cancel" : cancel
+    },
+    autoOpen : false,
+    show: {
+      effect: "explode",
+      duration: 500
+    },
+      hide: {
+        effect: "slide",
+        duration: 300
+
+      }
+  };
+
+  $("#reportDialog").dialog(dialogOpts);
+  $("#report").click(function() {
+    $("#correction").val("");
+    $("#reportDialog").dialog("open");
+    $("#marked").text(getSelectionText());
+  });
 });
 
-$(document).ready(function(){
-//  $("#dalog").hide();
 
-    $("#opener").click(function () {
-      alert("stuff");
-      $("#dialog").dialog("open");
-    });
-});
-
-//
-//function getSelectionText() {
-//  var text = "";
-//  if (window.getSelection) {
-//    text = window.getSelection().toString();
-//  } else if (document.selection && document.selection.type != "Control") {
-//    text = document.selection.createRange().text;
-//  }
-//  alert(text);
-//  return text;
-//}
-
-//When the button in the form is clicked, take the input value and set that as the value of `.myTarget`
-$('.formSaver').on('click', function () {
-  $('.myTarget').text($('.myInput').val());
-  $("#dialog").dialog('close');
-});
+function getSelectionText() {
+  var text = "";
+  if (window.getSelection) {
+    text = window.getSelection().toString();
+  } else if (document.selection && document.selection.type != "Control") {
+    text = document.selection.createRange().text;
+  }
+  return text;
+}
 
